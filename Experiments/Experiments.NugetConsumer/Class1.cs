907aa22f2d583e304dd7b016764e0b7f403cfe25
Carlos da Silva <dasilva.carlos@outlook.com>
2016-12-16 23:28:21 +0100

    Added nuget with powershell script executed as prebuild on the consumer

---
+++ Experiments/Experiments.NugetConsumer/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Experiments.NugetConsumer
+{
+    public class Class1
+    {
+    }
+}
