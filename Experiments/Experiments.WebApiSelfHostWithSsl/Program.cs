d4d6335bc9351b23d2376f8373ccf6606629d1c0
Carlos Filipe da Silva Oliveira <dasilva.carlos@outlook.com>
2017-06-19 23:23:38 +0200

    Added project showing how to self host a web api project based on Owin

---
+++ Experiments/Experiments.WebApiSelfHostWithSsl/Program.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Owin.Hosting;
+using System;
+using System.Net;
+using System.Net.Http;
+
+namespace Experiments.WebApiSelfHostWithSsl
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var baseServerAddress = "https://+:9443";
+            var baseAddress = "https://localhost:9443/";
+
+            // Start OWIN host
+            using (var x = WebApp.Start<Startup>(url: baseServerAddress))
+            {
+                // Create HttpCient and make a request to api/values
+                ServicePointManager.ServerCertificateValidationCallback += (sender, cert, chain, sslPolicyErrors) =>
+                {
+                    return true;
+                };
+                using (var client = new HttpClient())
+                {
+                    var response = client.GetAsync(baseAddress + "api/values").Result;
+                    Console.WriteLine(response);
+                    Console.WriteLine(response.Content.ReadAsStringAsync().Result);
+                }
+
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ Experiments/Experiments.WebApiSelfHostWithSsl/Scripts/Setup.ps1
@@ -0,0 +1,9 @@
+﻿$x = New-SelfSignedCertificate -DnsName localhost -CertStoreLocation Cert:\LocalMachine\My
+$guid = [guid]::NewGuid()
+$certHash = $x.Thumbprint
+$ip = "0.0.0.0" # This means all IP addresses
+$port = "9443" # the default HTTPS port
+"http add sslcert ipport=$($ip):$port certhash=$certHash appid={$guid}" | netsh
+
+
+"http add urlacl url=https://+:9443/ user=""[ENTER USERNAME WITH DOMAIN HERE]""" | netsh
